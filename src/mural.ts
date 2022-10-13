681c744f4b03531c9f83e61d2a00c496654d9e09
Gabriel Cecon Carlsen <cecon.carlsen@unesp.br>
2022-10-13 14:39:40 -0300

    refactor: suka blyat

--- src/mural.ts
+++ src/mural.ts
@@ -1,12 +1,11 @@
 type memberType = {
-  nome: string,
-  diretoria: string,
-  tecnologias: string,
-  sobreMim: string,
-}
+  nome: string;
+  diretoria: string;
+  tecnologias: string;
+  sobreMim: string;
+};
 
 type arrayMemberType = memberType[];
-
 
 export const members: arrayMemberType = [
   // {
@@ -18,13 +17,21 @@ export const members: arrayMemberType = [
   {
     nome: "karol marques",
     diretoria: "(a melhor delas) RH ❤️️❤️️",
-    tecnologias: "React.js, Flutter, Typescript, Java (bem de vez em quando porque né)",
-    sobreMim: "oi 👻 gosto de gatos e cerveja, apenas."
+    tecnologias:
+      "React.js, Flutter, Typescript, Java (bem de vez em quando porque né)",
+    sobreMim: "oi 👻 gosto de gatos e cerveja, apenas.",
   },
   {
     nome: "haria othon",
     diretoria: "Adm/Jur/Fin",
     tecnologias: "Nodejs, mongodb, Aws (tentando né :p), typescript.",
-    sobreMim: "Olá, sou diretor de Adm/Jur/Fin, estudante do 4ºAno (4!) em bacharel de Ciências da Computação."
+    sobreMim:
+      "Olá, sou diretor de Adm/Jur/Fin, estudante do 4ºAno (4!) em bacharel de Ciências da Computação.",
+  },
+  {
+    nome: "Gabriel Cecon",
+    diretoria: "Projetos",
+    tecnologias: "Typescript, ReactJs, ReactNative, NodeJs ",
+    sobreMim: "Xandão lover e apreciador de animais acima do peso",
   },
 ];
